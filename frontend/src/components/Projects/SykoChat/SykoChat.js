83e4daf3ae473b10429e692da3217b48e00f7734
SykoNaught <sykonaught@users.noreply.github.com>
2025-01-01 18:14:41 -0600

    fetch url oopsie

--- frontend/src/components/Projects/SykoChat/SykoChat.js
+++ frontend/src/components/Projects/SykoChat/SykoChat.js
@@ -30,7 +30,7 @@ const ChatBot = () => {
         setLoading(true); // Set loading to true when waiting for a response
 
         try {
-            const response = await fetch("http://sykonaught/api/chat", {
+            const response = await fetch("https://sykonaught/api/chat", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ message: input }),
